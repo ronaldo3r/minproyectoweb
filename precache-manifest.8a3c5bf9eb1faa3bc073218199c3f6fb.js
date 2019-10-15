26f77e05e0c309d0c4adec447aaef56617ef28ba
ronaldo3r <ronaldo3-0827@hotmail.com>
2019-10-15 16:01:21 -0500

    Updates

---
+++ precache-manifest.8a3c5bf9eb1faa3bc073218199c3f6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496ceeff9153fbe885ba9aa4c6eb5270",
+    "url": "/minproyectoweb/index.html"
+  },
+  {
+    "revision": "f751e9efd6ef91eb9e4d",
+    "url": "/minproyectoweb/static/css/main.a9827aa0.chunk.css"
+  },
+  {
+    "revision": "ad31b72bd5011f143f98",
+    "url": "/minproyectoweb/static/js/2.713d24d5.chunk.js"
+  },
+  {
+    "revision": "f751e9efd6ef91eb9e4d",
+    "url": "/minproyectoweb/static/js/main.d20e0aaa.chunk.js"
+  },
+  {
+    "revision": "544a1efd4c6cd30353e0",
+    "url": "/minproyectoweb/static/js/runtime-main.2cb807b3.js"
+  }
+]);
